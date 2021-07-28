c0ec01d99af64755f63dcf250d586bdb47f901b7
Viktor Mishyn <viktormishyn@gmail.com>
2021-07-28 19:22:53 +0300

    Add lambda to aws-cdk-workshop-ts-stack.ts

--- lib/aws-cdk-workshop-ts-stack.ts
+++ lib/aws-cdk-workshop-ts-stack.ts
@@ -1,10 +1,14 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class AwsCdkWorkshopTsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"), // 'lambda' directory
+      handler: "hello.handler",
+    });
   }
 }

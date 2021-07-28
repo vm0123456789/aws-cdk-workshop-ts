0b710f81d6ebda207650bd0397e997f13a56ae3c
Viktor Mishyn <viktormishyn@gmail.com>
2021-07-28 20:00:50 +0300

    Add APIGateway endpoint

--- lib/aws-cdk-workshop-ts-stack.ts
+++ lib/aws-cdk-workshop-ts-stack.ts
@@ -5,10 +5,15 @@ import * as apigw from "@aws-cdk/aws-apigateway";
 export class AwsCdkWorkshopTsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
     const hello = new lambda.Function(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset("lambda"), // 'lambda' directory
       handler: "hello.handler",
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
     });
   }
 }
